79a4f8efe4449eeb09511106bfcd398dca35a439
Cade Gordon <cadergator1@hotmail.com>
2024-08-10 21:15:08 +0000

    plotting on these plots

--- experiments/dms/all-vs-wt.py
+++ experiments/dms/all-vs-wt.py
@@ -1,19 +1,15 @@
 import os
-from pathlib import Path
 
 import click
-import numpy as np
-import pandas as pd
 
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 from transformers import DataCollatorWithPadding
 from datasets import Dataset
 import torch
+import pandas as pd
 
 from kronfluence import Analyzer, prepare_model
 from kronfluence.utils.dataset import DataLoaderKwargs
-from kronfluence.utils.state import release_memory
-from transformers import AutoTokenizer, AutoModelForMaskedLM
 
 from bio_if.tasks import ESMMLMTask
 
@@ -24,11 +20,18 @@ from bio_if.tasks import ESMMLMTask
 
 AMINO_ACIDS = "ACDEFGHIKLMNPQRSTVWY"
 
+
 @click.command()
-@click.option('--model_name', default="facebook/esm2_t12_35M_UR50D", help='Model name', type=str)
-@click.option('--seed', default=0, help='Seed', type=int)
-@click.option('--per_device_batch_size', default=128, help='Per device batch size', type=int)
-@click.option('--study_name', default='GCN4_YEAST_Staller_2018', help='Study name', type=str)
+@click.option(
+    "--model_name", default="facebook/esm2_t12_35M_UR50D", help="Model name", type=str
+)
+@click.option("--seed", default=0, help="Seed", type=int)
+@click.option(
+    "--per_device_batch_size", default=128, help="Per device batch size", type=int
+)
+@click.option(
+    "--study_name", default="GCN4_YEAST_Staller_2018", help="Study name", type=str
+)
 def main(model_name: str, seed: int, per_device_batch_size: int, study_name: str):
     is_ddp = os.environ.get("WORLD_SIZE") is not None
     if is_ddp:
@@ -37,29 +40,32 @@ def main(model_name: str, seed: int, per_device_batch_size: int, study_name: str
         torch.distributed.init_process_group(backend="nccl")
     else:
         local_rank = 0
-    
+
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForMaskedLM.from_pretrained(model_name, device_map=f'cuda:{local_rank}')
-    tokenizer.model_max_length = float('inf')
+    model = AutoModelForMaskedLM.from_pretrained(
+        model_name, device_map=f"cuda:{local_rank}"
+    )
+    tokenizer.model_max_length = float("inf")
     # remove model.esm.contact_head from model
     del model.esm.contact_head
 
-    all_dms_df = pd.read_csv('DMS_substitutions.csv')
-    seq = all_dms_df[all_dms_df['DMS_id'] == study_name]['target_seq'].iloc[0]
-    seqs_df = pd.read_csv(f'studies/{study_name}.csv')
+    all_dms_df = pd.read_csv("DMS_substitutions.csv")
+    seq = all_dms_df[all_dms_df["DMS_id"] == study_name]["target_seq"].iloc[0]
+    seqs_df = pd.read_csv(f"studies/{study_name}.csv")
     # sample 1000 sequences without replacement
     seqs_df = seqs_df.sample(n=1000, random_state=seed, replace=False)
-    seqs = seqs_df['mutated_sequence'].tolist()
+    seqs = seqs_df["mutated_sequence"].tolist()
 
-    ds = Dataset.from_dict({'seq': seqs})
-    ds = ds.map(lambda x: tokenizer(x['seq']))
+    ds = Dataset.from_dict({"seq": seqs})
+    ds = ds.map(lambda x: tokenizer(x["seq"]))
 
-    test_ds = Dataset.from_dict({'seq': [seq]})
-    test_ds = test_ds.map(lambda x: tokenizer(x['seq']))
-
+    test_ds = Dataset.from_dict({"seq": [seq]})
+    test_ds = test_ds.map(lambda x: tokenizer(x["seq"]))
 
     collator_fn = DataCollatorWithPadding(tokenizer)
-    task = ESMMLMTask(tokenizer.all_special_ids, num_layers=len(model.esm.encoder.layer))
+    task = ESMMLMTask(
+        tokenizer.all_special_ids, num_layers=len(model.esm.encoder.layer)
+    )
     model = prepare_model(model, task)
     if is_ddp:
         model = torch.nn.parallel.DistributedDataParallel(model)
@@ -71,7 +77,6 @@ def main(model_name: str, seed: int, per_device_batch_size: int, study_name: str
         model=model,
         cpu=False,
     )
-
 
     dataloader_kwargs = DataLoaderKwargs(collate_fn=collator_fn)
     analyzer.set_dataloader_kwargs(dataloader_kwargs)
@@ -87,13 +92,32 @@ def main(model_name: str, seed: int, per_device_batch_size: int, study_name: str
     analyzer.compute_pairwise_scores(
         scores_name=f"{model_name}_{study_name}",
         factors_name=f"{model_name}_{study_name}",
-        overwrite_output_dir=True,
+        overwrite_output_dir=False,
         train_dataset=ds.select_columns(["input_ids", "attention_mask"]),
         query_dataset=test_ds.select_columns(["input_ids", "attention_mask"]),
         per_device_query_batch_size=1,
         per_device_train_batch_size=per_device_batch_size,
     )
 
+    # recursively delete the factors_facebook folder
+    if local_rank == 0:
+        path = f"analyses/{model_name}_{study_name}_seed={seed}/factors_facebook"
+        assert os.path.exists(path)
+        if os.path.exists(path):
+            # Walk through the directory
+            for root, dirs, files in os.walk(path, topdown=False):
+                # Delete all files
+                for file in files:
+                    os.remove(os.path.join(root, file))
+                # Delete all directories
+                for dir in dirs:
+                    os.rmdir(os.path.join(root, dir))
+            # Delete the main directory
+            os.rmdir(path)
+            print(f"Successfully deleted the folder: {path}")
+        else:
+            print(f"The folder does not exist: {path}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- experiments/dms/yeast.bash
+++ experiments/dms/yeast.bash
@@ -2,25 +2,29 @@
 # model_name = "facebook/esm2_t33_650M_UR50D"
 # model_name = "facebook/esm2_t30_150M_UR50D"
 # model_name = "facebook/esm2_t12_35M_UR50D"
+# model_name = "facebook/esm2_t6_8M_UR50D"
 
 # Array of study names
 study_names=(
-    "Q8WTC7_9CNID_Somermeyer_2022"
+    # "GCN4_YEAST_Staller_2018"
+    # "Q8WTC7_9CNID_Somermeyer_2022"
     "CAPSD_AAV2S_Sinai_2021"
-    "HIS7_YEAST_Pokusaeva_2019"
-    "D7PM05_CLYGR_Somermeyer_2022"
-    "GFP_AEQVI_Sarkisyan_2016"
-    "PHOT_CHLRE_Chen_2023"
-    "Q6WV13_9MAXI_Somermeyer_2022"
-    "F7YBW7_MESOW_Ding_2023"
-    "F7YBW8_MESOW_Aakre_2015"
+    # "HIS7_YEAST_Pokusaeva_2019"
+    # "D7PM05_CLYGR_Somermeyer_2022"
+    # "GFP_AEQVI_Sarkisyan_2016"
+    # "PHOT_CHLRE_Chen_2023"
+    # "Q6WV13_9MAXI_Somermeyer_2022"
+    # "F7YBW7_MESOW_Ding_2023"
+    # "F7YBW8_MESOW_Aakre_2015"
 )
 
 # Loop through each study name and run the commands
 for study_name in "${study_names[@]}"
 do
-    CUDA_VISIBLE_DEVICES=4,5,6,7 torchrun --standalone --nproc-per-node=4 all-vs-wt.py --per_device_batch_size=8 --model_name="facebook/esm2_t12_35M_UR50D" --study_name="$study_name"
-    CUDA_VISIBLE_DEVICES=4,5,6,7 torchrun --standalone --nproc-per-node=4 all-vs-wt.py --per_device_batch_size=8 --model_name="facebook/esm2_t30_150M_UR50D" --study_name="$study_name"
-    CUDA_VISIBLE_DEVICES=4,5,6,7 torchrun --standalone --nproc-per-node=4 all-vs-wt.py --per_device_batch_size=8 --model_name="facebook/esm2_t33_650M_UR50D" --study_name="$study_name"
-    CUDA_VISIBLE_DEVICES=4 torchrun --standalone --nproc-per-node=1 all-vs-wt.py --per_device_batch_size=8 --model_name="facebook/esm2_t36_3B_UR50D" --study_name="$study_name"
+    # CUDA_VISIBLE_DEVICES=1,2,3,4 torchrun --standalone --nproc-per-node=4 all-vs-wt.py --per_device_batch_size=8 --model_name="facebook/esm2_t6_8M_UR50D" --study_name="$study_name"
+    # CUDA_VISIBLE_DEVICES=1,2,3,4 torchrun --standalone --nproc-per-node=4 all-vs-wt.py --per_device_batch_size=8 --model_name="facebook/esm2_t12_35M_UR50D" --study_name="$study_name"
+    # CUDA_VISIBLE_DEVICES=1,2,3,4 torchrun --standalone --nproc-per-node=4 all-vs-wt.py --per_device_batch_size=8 --model_name="facebook/esm2_t30_150M_UR50D" --study_name="$study_name"
+    # CUDA_VISIBLE_DEVICES=1,2,3,4 torchrun --standalone --nproc-per-node=4 all-vs-wt.py --per_device_batch_size=8 --model_name="facebook/esm2_t33_650M_UR50D" --study_name="$study_name"
+    # CUDA_VISIBLE_DEVICES=1,2,3,4 torchrun --standalone --nproc-per-node=4 all-vs-wt.py --per_device_batch_size=4 --model_name="facebook/esm2_t36_3B_UR50D" --study_name="$study_name"
+    CUDA_VISIBLE_DEVICES=2,3,4,5 torchrun --standalone --nproc-per-node=4 all-vs-wt.py --per_device_batch_size=1 --model_name="facebook/esm2_t36_3B_UR50D" --study_name="$study_name"
 done
